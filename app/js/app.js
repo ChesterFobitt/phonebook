7e6a1cbe4ecb48a34282b90854f8175371920d78
Maxim Filippovsky <m.filippovsky@i-sys.ru>
2017-06-05 19:34:54 +0400

    [~] refactoring, comments

--- app/js/app.js
+++ app/js/app.js
@@ -1,26 +1,33 @@
 (function(){
   'use strict';
 
-  var app = angular.module( 'app', [ 'ui.bootstrap' ] )
+  var app = angular.module( 'app', [ 'ui.bootstrap' ] );
 
   app.controller('phonebookCtrl', PhonebookController);
 
   function PhonebookController($scope, $uibModal) {
 
+    // Получаем или создаем запись в локальном хранилище в формате json
     $scope.phonebook = localStorage.getItem('Phonebook');
     $scope.phonebook = $scope.phonebook ? JSON.parse($scope.phonebook) : [];
+    
     $scope.reverse = false;
     $scope.sortingField = 'name';
-
+    
+    // Переключение направления сортировки 
     $scope.reverseSort = function () {
       $scope.reverse = !$scope.reverse;
     }
+
+    // Открытие модального окна 
     $scope.openModalForm = function(){
       $uibModal.open({
           templateUrl: 'modal-form.html',
           scope: $scope
       });
     }
+
+    // Добавление элемента в список
     $scope.addUser = function(user) {    
       var newId = new Date().getTime();
       $scope.phonebook.push({
@@ -28,9 +35,12 @@
         name: user.name,
         phone: user.phone
       });
+      
+      // Очистка полей
       user.name = '';
       user.phone = '';
-      
+
+      // Обновляем локальное хранилище
       localStorage.setItem('Phonebook', JSON.stringify($scope.phonebook));
     }
     $scope.deleteUser = function( id ) {
@@ -40,6 +50,7 @@
         }
       });
 
+      // Обновляем локальное хранилище
       localStorage.setItem('Phonebook', JSON.stringify($scope.phonebook));
     }
   }
